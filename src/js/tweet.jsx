874c92f8044df61e7217dac3bd3c9c916d047b2b
BenoitArguel <benoit.arguel@gmail.com>
2016-05-13 17:04:32 +0200

    feat: adding movement (work in progress)

--- src/js/tweet.jsx
+++ src/js/tweet.jsx
@@ -1,45 +1,54 @@
 import React, {Component, PropTypes} from 'react'; 
 import Fountain from './fountain.jsx';
-import TWEEN from 'tween.js';
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var animationTime = 400;
+var frames = ( animationTime / 1000 ) * 60;
+var lenghtX = 200;
+var lenghtY = random(100,300);
+var oneFrame = (animationTime / frames) / (animationTime / lenghtX);
+
+
+//  x: x, t: current time, b: begInnIng value, c: change In value, d: duration
+function easeOutCubic(x, t, b, c, d) {
+  return c*((t=t/d-1)*t*t + 1) + b;
+}
+function easeInCubic(x, t, b, c, d) {
+  return c*(t/=d)*t*t + b;
+}
 
 class Tweet extends Component { 
-
-	constructor(props){
-		super(props);
-		this.state = {
-			x: 0,
-			y: 0
-		};
-	}
-	componentDidMount(){
-		this.startTime = Date.now();
-		this.animationInterval = setInterval(() => this.updatePosition(), 1000 / 60);
-	}
-	componentWillUnmoun(){
-		clearInterval(this.animationInterval);
-	}
-	updatePosition(){
+  constructor(props){
+    super(props);
+    this.state = {
+      x: 0,
+      y: 0
+    };
+  }
+  componentDidMount(){
+    this.startTime = Date.now();
+    this.animationInterval = setInterval(() => this.updatePosition(), 1000 / 60);
+  }
+  componentWillUnmoun(){
+    clearInterval(this.animationInterval);
+  }
+  updatePosition(){
     var myTime = Date.now() - this.startTime;
-    var animationTime = 500;
-    var frames = ( animationTime / 1000 ) * 60;
-    var lenghtX = 400;
-    var lenghtY = lenghtX * 2;
-    var xOneFrame = (animationTime / frames) / (animationTime / lenghtX);
-    var yOneFrame = ((animationTime / frames) / (animationTime / lenghtY));
-    
     if (myTime < animationTime) {
       this.setState({
-        x: this.state.x + xOneFrame,
-        y: ( this.state.y - yOneFrame ) * 0.9
+        x: this.state.x + oneFrame,
+        y: easeOutCubic(lenghtX, myTime, 0, lenghtY, animationTime)
       });
-    } if (myTime > animationTime) {
+    } else if (myTime > animationTime && myTime < animationTime*2) {
       this.setState({
-        x: this.state.x + xOneFrame,
-        y: this.state.y * 0.9 
+        x: this.state.x + oneFrame,
+        y: easeInCubic(lenghtX, myTime - animationTime, lenghtY, 0, animationTime)
       });
     }
   }
-
 
 
 	render() {
@@ -62,7 +71,7 @@ class Tweet extends Component {
 	    position: 'absolute',
 	    marginTop: heightContainer,
 	    marginLeft: ( widthContainer / 2 ) - 24,
-	    transform: 'translateX('+ this.state.x +'px) translateY('+ this.state.y +'px)'
+	    transform: 'translateX('+ this.state.x +'px) translateY('+ - this.state.y +'px)'
 	  };
 	  return <div style={styleTweet} className="tweet" style={styleTweet}>
       <img src={profilePicture} style={styleImg} />
